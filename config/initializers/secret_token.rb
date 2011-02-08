640783a228037ac431809cac7ef74c18337ce4a3
Sven Pedersen <spe@miracleas.dk>
2011-02-08 09:44:16 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Regulate::Application.config.secret_token = '9b3d38d8d37d4f1c5f52e11cc55d9564cbe9dc0cf85fdda94396e8299bc319591862eb94077a7aacd7743da02efc43a7c592fdd94757c40f0a1489a03dc6fee8'
